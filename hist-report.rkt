510dedf6c410b7086aa2cd9424bd76bbabf3f453
Nixie <onixie@gmail.com>
2012-07-05 21:28:24 +0800

    Add Histogram compactness optimization

--- hist-report.rkt
+++ hist-report.rkt
@@ -3,15 +3,17 @@
 (require plot)
 (provide make-hist
          class-hist
-         plot-hist)
+         plot-hist
+         optimize-hist
+         compact-hist)
 
-(struct class (low high) 
+(struct class (low high)
   #:property prop:custom-write (lambda (stt p m) 
                                  (fprintf p "~a~~~a" 
                                           (class-low stt) 
                                           (class-high stt))))
 
-(struct class-0s (low high)
+(struct class-0s class ()
   #:property prop:custom-write (lambda (stt p m)
                                  (fprintf p "...")))
 
@@ -32,11 +34,28 @@
        hist)
   hist)
 
-(define (optimize-hist hist)
-  (let find-zero ((h hist) (s 0) (e 0) (zs null))
-    (cond ((null? h) (if (= s e) zs (cons (list s (sub1 e)) zs)))
-          ((zero? (vector-ref (car h) 1)) (find-zero (rest h) s (add1 e) zs))
-          (else (find-zero (rest h) (+ e 2) (+ e 2) (if (= s e) zs (cons (list s e) zs)))))))
+(define (optimize-hist hist th)
+  (define (etc. oh eh)
+    (cond ((null? eh) oh)
+          ((< (length eh) th)
+           (append eh oh))
+          (else (cons (vector (class-0s (class-low (if (null? oh)
+                                                       (vector-ref (car hist) 0)
+                                                       (vector-ref (car oh) 0)))
+                                        (class-high (vector-ref (car eh) 0)))
+                              0)
+                      oh))))
+  (let find-zero ((h hist) (oh null) (eh null))
+    (cond ((null? h) (reverse (etc. oh eh)))
+          ((zero? (vector-ref (car h) 1)) (find-zero (rest h) oh (cons (car h) eh)))
+          (else (find-zero (rest h) 
+                           (cons (car h) (etc. oh eh))
+                           null)))))
+
+(define (compact-hist hist)
+  (filter (lambda (class)
+            (not (zero? (vector-ref class 1))))
+          hist))
   
 (define (plot-hist hist #:width-ratio (width-ratio 1) #:title (title #f) #:x-label (x-label #f) #:y-label (y-label #f))
   (plot (discrete-histogram hist) 
